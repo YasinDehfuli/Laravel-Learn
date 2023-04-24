39d9aeb59f75b3da56bdcdc2cd34623015844c1b
Nisay <110921275+YasinDehfuli@users.noreply.github.com>
2023-04-24 10:41:48 +0330

    Laravel Learn Sample To Start Learn Laravel

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about',function (){
+return 'we are in about page';
+});
+
+Route::get('/home/student/login/who',function (){
+return '<h1> Welcome Here </h1>';
+});
